85b814c7dbdc4122c41dea3f4ecdb1bf59a7dd13
Roger Morris <rogamorris@gmail.com>
2023-01-07 14:40:47 -0500

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Define a function that accepts a string called "string"
+    // Initialize a variable called "output" to an empty string
+    // Initialize a variable called "numIterations" to the length of "string"
+    // Iterate for "numIterations":
+    //      slice the letter in the last position minus the iteration number and add it to "output"
+    // return "output"
+
+    let output = "";
+    
+    let numIterations = string.length;
+    for (let i = 0; i < numIterations; i++) {
+        output += string.slice(numIterations - (1 + i), numIterations - i);
+        console.log(output);
+    }
+
+    return output;
 
 };
 
