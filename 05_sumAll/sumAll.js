f57ddff2306bbfa36a60a7b3519745e23c148c6e
Roger Morris <rogamorris@gmail.com>
2023-01-09 20:26:13 -0600

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // check if int1 and int2 are positive and numbers
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    } else if (typeof int1 != 'number' || typeof int2 != 'number') {
+        return "ERROR";
+    };
 
+    let biggerInt;
+    let smallerInt;
+    let output = 0;
+
+    if (int1 > int2) {
+        biggerInt = int1;
+        smallerInt = int2;
+    } else if (int2 > int1) {
+        biggerInt = int2;
+        smallerInt = int1;
+    } else {
+        output = int1 + int2;
+    }
+
+    for (let i = smallerInt; i <= biggerInt; i++) {
+        output += i;
+    }
+
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
