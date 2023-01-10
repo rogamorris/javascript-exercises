5eb0b3bf7ca1184d7a362f326e032bc5e9598c90
Roger Morris <rogamorris@gmail.com>
2023-01-10 16:53:03 -0600

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+    array = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (arguments[i] === array[j]) {
+                array.splice(j, 1);
+            };
+        };
+    };
+
+    return array;
 };
 
 // Do not edit below this line
