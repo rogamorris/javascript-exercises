54bc901a7373b719a42dde1cc02ce6f232c11ccf
Roger Morris <rogamorris@gmail.com>
2023-01-10 17:18:01 -0600

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let unrounded = (tempF - 32) * (5/9);
+  return Math.round(unrounded * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let unrounded = (tempC * (9/5) + 32);
+  return Math.round(unrounded * 10) / 10;
 };
 
 // Do not edit below this line
